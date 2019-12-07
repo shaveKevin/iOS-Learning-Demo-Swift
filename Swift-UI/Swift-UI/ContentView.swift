7de7166ca395135ebe8e8d72211ca410fd8adc0c
shavekevin <shavekevin@gmail.com>
2019-12-07 11:41:30 +0800

    init

---
+++ Swift-UI/Swift-UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-UI
+//
+//  Created by shavekevin on 2019/12/7.
+//  Copyright © 2019 shavekevin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
